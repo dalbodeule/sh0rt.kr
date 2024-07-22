425653feef40a02015953f4d5b37a2dfe3243747
dalbodeule <11470513+dalbodeule@users.noreply.github.com>
2024-07-22 20:50:25 +0900

    done?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,34 +19,34 @@ export default defineNuxtConfig({
     '@fortawesome/fontawesome-svg-core/styles.css'
   ],
   runtimeConfig: {
-    analyticsAccountId: process.env.ANALYTICS_ACCOUNT_ID,
-    analyticsApiToken: process.env.ANALYTICS_API_TOKEN,
-    domainZoneId: process.env.DOMAIN_ZONE_ID,
-    domainApiToken: process.env.DOMAIN_API_TOKEN,
-    domainApiEmail: process.env.DOMAIN_API_EMAIL,
-    apiBackend: process.env.API_BACKEND,
+    analyticsAccountId: process.env.NUXT_ANALYTICS_ACCOUNT_ID,
+    analyticsApiToken: process.env.NUXT_ANALYTICS_API_TOKEN,
+    domainZoneId: process.env.NUXT_DOMAIN_ZONE_ID,
+    domainApiToken: process.env.NUXT_DOMAIN_API_TOKEN,
+    domainApiEmail: process.env.NUXT_DOMAIN_API_EMAIL,
+    apiBackend: process.env.NUXT_API_BACKEND,
     session: {
-      password: process.env.SESSION_PASSWORD
+      password: process.env.NUXT_SESSION_PASSWORD
     },
     oauth: {
       github: {
-        clientId: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        clientId: process.env.NUXT_GITHUB_CLIENT_ID,
+        clientSecret: process.env.NUXT_GITHUB_CLIENT_SECRET,
       },
       google: {
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        clientId: process.env.NUXT_GOOGLE_CLIENT_ID,
+        clientSecret: process.env.NUXT_GOOGLE_CLIENT_SECRET,
       }
     },
     public: {
-      baseUrl: process.env.BASE_URL,
-      domainList: process.env.DOMAIN_LIST,
-      domainLimit: process.env.DOMAIN_LIMIT,
+      baseUrl: process.env.NUXT_BASE_URL,
+      domainList: process.env.NUXT_DOMAIN_LIST,
+      domainLimit: process.env.NUXT_DOMAIN_LIMIT,
     }
   },
   turnstile: {
-    secretKey: process.env.TURNSTILE_SECRET_KEY,
-    siteKey: process.env.TURNSTILE_SITE_KEY,
+    secretKey: process.env.NUXT_TURNSTILE_SECRET_KEY,
+    siteKey: process.env.NUXT_TURNSTILE_SITE_KEY,
   },
   hub: {
     analytics: true,
